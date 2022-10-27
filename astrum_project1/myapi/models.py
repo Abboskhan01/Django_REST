4bba151d76be850605b450065ebf5e20900fe48c
Abbos <abbosr180@gmail.com>
2022-10-27 18:00:45 +0500

    added new project

--- astrum_project1/myapi/models.py
+++ astrum_project1/myapi/models.py
@@ -24,8 +24,8 @@ class Category(models.Model):
 
 
 class Books(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
     class Meta:
