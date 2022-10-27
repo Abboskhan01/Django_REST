ff61b9048e35703a96684c267d4e341aafb3d402
Abbos <abbosr180@gmail.com>
2022-10-27 19:24:19 +0500

    added new project

--- astrum_project2/app2/urls.py
+++ astrum_project2/app2/urls.py
@@ -1,10 +1,21 @@
-from rest_framework import routers
-from .views import AuthorViewSet
+from rest_framework import routers, generics
+# from .views import AuthorViewSet
+from .models import Author
+from .serializers import AuthorSerializer
 from django.urls import path, include
+from .views import *
 
-router = routers.DefaultRouter()
-router.register(r'authors', AuthorViewSet)
-
+# router = routers.DefaultRouter()
+# router.register(r'authors', AuthorViewSet)
+#
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
 urlpatterns = [
-    path('', include(router.urls))
+    path('authors/', AuthorListView.as_view(), name='Authors_list'),
+    path('authors/<pk>/', AuthorRetrieveView.as_view(), name='Author'),
+    # path('authors/<pk>', AuthorDestroyView.as_view(), name='Author'),
+    # path('authors/<pk>', AuthorCreateView.as_view(), name='Author'),
+    # path('authors/<pk>', AuthorUpdateView.as_view(), name='Author'),
+    path('user/', generics.ListCreateAPIView.as_view(queryset=Author.objects.all(), serializer_class=AuthorSerializer))
 ]
--- astrum_project2/app2/views.py
+++ astrum_project2/app2/views.py
@@ -1,8 +1,33 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 from .models import Author
 from .serializers import AuthorSerializer
 
 
-class AuthorViewSet(ModelViewSet):
+# class AuthorViewSet(ModelViewSet):
+#     queryset = Author.objects.all()
+#     serializer_class = AuthorSerializer
+
+class AuthorListView(generics.ListAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorRetrieveView(generics.RetrieveAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorUpdateView(generics.UpdateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorDestroyView(generics.DestroyAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
+
+
+class AuthorCreateView(generics.CreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
